acea1ca4060310107861ec04914f09de8eb9be71
Manish Sharma <manishsharma18772@gmail.com>
2026-08-29 15:42:36 +0000

    Update

--- hbs_crm/patches/update_desktop_icon.py
+++ hbs_crm/patches/update_desktop_icon.py
@@ -3,6 +3,9 @@ import frappe
 def execute():
 	# Forcefully update the Desktop Icon value in the database
 	if frappe.db.exists("Desktop Icon", "Hbs Crm"):
-		frappe.db.set_value("Desktop Icon", "Hbs Crm", "link_type", "Workspace Sidebar")
+		frappe.db.set_value("Desktop Icon", "Hbs Crm", {
+			"link_type": "External",
+			"link": "/app/hbs-crm"
+		})
 		frappe.clear_cache(doctype="Desktop Icon")
 		frappe.clear_cache()
